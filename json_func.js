3a9bcdf322a2646a8b36849893960d3991d959a5
stevenvp05 <stevenvanpopele@hotmail.com>
2023-03-23 22:00:22 +0100

    Start of custom datastructure.

--- json_func.js
+++ json_func.js
@@ -1,3 +1,14 @@
+class Node {
+    constructor() {
+        this.key = 0;
+        this.label = null;
+        this.refinement = null;
+        this.parent = null;
+        this.children = [];
+        this.amount = 0;
+    }
+}
+
 // Function for returning JSON
 function getJson(temp) { 
     if (temp == 0){
@@ -100,17 +111,205 @@ async function getXML(){
     return xml;
 }
 
+// function parse_node(node) {
+//     var json_node = {};
+
+//     // Parse the label for the current node
+//     var label = node.getElementsByTagName("label")[0];
+//     json_node.label = label.textContent;
+
+//     // Parse the refinement for the current node
+//     var refinement = node.getAttribute("refinement");
+//     if (refinement) {
+//         json_node.refinement = refinement;
+//     }
+
+//     // Parse any parameters for the current node
+//     var parameters = node.getElementsByTagName("parameter");
+//     if (parameters.length > 0) {
+//         json_node.parameters = {};
+//         for (var i = 0; i < parameters.length; i++) {
+//             var parameter = parameters[i];
+//             var domainId = parameter.getAttribute("domainId");
+//             var value = parameter.textContent;
+//             json_node.parameters[domainId] = value;
+//         }
+//     }
+
+//     // Recursively parse child nodes
+//     var child_nodes = node.getElementsByTagName("node");
+//     if (child_nodes.length > 0) {
+//         json_node.children = [];
+//         for (var i = 0; i < child_nodes.length; i++) {
+//             var child_node = child_nodes[i];
+//             json_node.children.push(parse_node(child_node));
+//         }
+//     }
+
+//     return json_node;
+// }
+
+// // Builds the json object as a string
+// async function build_json(input_text) {
+//     var parser = new DOMParser();
+//     var xml = parser.parseFromString(input_text, "text/xml");
+
+//     // Parse the root node of the adtree
+//     var adtree_node = new Node(xml.getElementsByTagName("adtree")[0]);
+//     var json = parse_node(adtree_node);
+
+//     console.log(JSON.stringify(json, null, 2));
+//     return json;
+// }
+
+// // Add refinement and switchroles
+// async function ref_swi(item, j){
+//     var temp;
+//     while (item[j] != "r"){ // Find the refinement
+//         j++;
+//     }
+//     j += 12; // Position of refinement
+//     if (item[j] == "c"){
+//         temp = '"refinement": "conjunctive",'
+//     }
+//     else{
+//         temp = '"refinement": "disjunctive",'
+//     }
+//     j += 13 // Position of switchRole
+//     if (item[j] == "s"){
+//         temp += '"switchRole": "yes",';
+//     }
+//     return temp;
+// }
+
+// function printInorder(node) {
+//     if (node == null)
+//         return;
+
+//     /* first recur on left child */
+//     printInorder(node.node_left);
+
+//     /* then print the data of node */
+//     document.write(node.node_name + " ");
+
+//     /* now recur on right child */
+//     printInorder(node.node_right);
+// }
+
+// // Builds temporary datastructure
+// async function temp_dstr(xml){
+//     var root = null;
+
+//     var root = xml.getElementsByTagName("adtree")[0];
+
+//     return tree_in_list;
+
+//     root = new Node(1);
+//     root.node_left = new Node(2);
+//     root.node_right = new Node(3);
+//     root.node_left.node_left = new Node(4);
+//     root.node_left.node_right = new Node(5);
+//     document.write("<br/>Inorder traversal of binary tree is <br/>");
+//     printInorder(root);
+// }
+
+async function add_Node(key, label, refinement){
+    var node = new Node();
+    node.key = key;
+    node.label = label;
+    node.refinement = refinement;
+
+    return node;
+}
+
+async function insert(root, key, label, refinement, depth, last_node){
+    var child = add_Node(key, label, refinement);
+    var x = root;
+    var y = null;
+
+    while (x != null){
+        y = x;
+        if (key % depth*2 == 0)
+            x = x.children[];
+        else
+            x = x.left;
+    }
+
+    // root = null tree is empty
+    // new node is root node
+    if (y == null)
+        y = child; 
+    // If new key is less than leaf node key
+    // Assign new node as its left child
+    else if (key % 2 == 0){
+        y.right = child;
+    }
+    // else assign new node as its  right child
+    else{
+        y.left = child;
+    }
+
+    return y;
+}
+
+async function find_label(item, j){
+    label = "";
+    j = j + 11;
+    while (item[j] != "<"){ // Retrieve the label
+        label += item[j];
+        j++;
+    }
+    return label;
+}
+
+async function find_refinement(item, j){
+    var refinement;
+    while (item[j] != "r"){ // Find the refinement
+        j++;
+    }
+    j += 12; // Position of refinement
+    if (item[j] == "c"){
+        refinement = "conjunctive";
+    }
+    else{
+        refinement = "disjunctive"
+    }
+    // j += 13 // Position of switchRole
+    // if (item[j] == "s"){
+    //     temp += '"switchRole": "yes",';
+    // }
+    return refinement;
+}
+
+function Inorder(root)
+{
+    if (root == null)
+        return;
+    else
+    {
+        Inorder(root.left);
+        console.log(root.key +" ");
+        Inorder(root.right);
+    }
+}
+
 // Builds the json object as a string
 async function build_json(input_text){
     const items = input_text.split("\n"); // Put the XML lines into a list of strings
-    var json = "{"; // String where in the json object will be build
     var item; // Single line of the XML file
     var temp = ""; // Temporary variable to save label names
     var j, k; // Counting variables
     var depth = 0; // Keeping track of the depth of the json
     var last = 0; // Did we close the last node? 0 means yes, 1 means no
+    var root = null; // Root of ADT init
+    var key = 0;
+    var seen = [];
+    var last_node = null;
+
     for (var i = 1; i < items.length; i++){ // Loop through all lines
         item = items[i];
+        console.log(depth);
+        console.log(item);
         j = 0;
         while (item[j] != "<"){ // Find the first useful character
             j++
@@ -118,126 +317,39 @@ async function build_json(input_text){
         switch(item[j + 1]){ // Every tag has to be treated differently
             case "/": // Closing tag
                 depth--;
-                if (item[j + 2] == "n"){ // Node closing tag? (nodes and parameters use [ & ] and need a different closing tag in json)
-                    json += '}]';
-                    last = 0;
-                }
-                else{
-                    json += '}';
-                }
                 break;
             case "a": // Adtree tag
-                depth++;
-                json += '"adtree": {';
                 break;
             case "n": // Node tag
                 depth++;
-                if (depth > 2){
-                    json += ", ";
+                k = 1
+                while (seen[depth*k] != null){
+                    k++
                 }
-                if (last == 0){
-                    json += '"node": [ {';
-                    last = 1;
+                seen[depth*k] = 1;
+                key = depth*k;
+                label = find_label(items[i+1], j);
+                refinement = find_refinement(item, j);
+                if (root == null){
+                    root = insert(root, key, label, refinement, depth, null);
+                    //console.log(root);
                 }
-                else{
-                    json += '{'
-                }
-                json += await ref_swi(item, j); // Add refinement and switchroles
-                break;
-            case "l": // Label tag
-                temp = "";
-                k = j + 7; // Position of label
-                while (item[k] != "<"){ // Retrieve the label
-                    temp += item[k];
-                    k++;
-                }
-                json += '"label": ' + '"' + temp + '"';
+                else
+                    last_node = insert(root, key, label, refinement, depth, last_node);
                 break;
             case "p": // Parameter tag
                 break;
             default: // Do nothing
         }
     }
-    json += "}"
-    console.log(json);
-    return json;
-}
-
-// Add refinement and switchroles
-async function ref_swi(item, j){
-    var temp;
-    while (item[j] != "r"){ // Find the refinement
-        j++;
-    }
-    j += 12; // Position of refinement
-    if (item[j] == "c"){
-        temp = '"refinement": "conjunctive",'
-    }
-    else{
-        temp = '"refinement": "disjunctive",'
-    }
-    j += 13 // Position of switchRole
-    if (item[j] == "s"){
-        temp += '"switchRole": "yes",';
-    }
-    return temp;
+    console.log(root);
+    return root;
 }
 
 //Converts XML to JSON and JSON to XML
 async function convert(XorJ){
     var input_text
     if (XorJ == 0){ // XorJ == 0 gives that input_file contains a XML
-        /*
-        input_text = [ // Declare XML as list of strings
-            '<?xml version="1.0" encoding="UTF-8"?>',
-            '<adtree>',
-            '    <node refinement="conjunctive">',
-            '        <label>Bank Account</label>',
-            '        <node refinement="disjunctive">',
-            '            <label>learn PIN</label>',
-            '            <node refinement="disjunctive">',
-            '                <label>eavesdrop PIN</label>',
-            '                <parameter domainId="MinTimeSeq1">10.0</parameter>',
-            '                <parameter domainId="ProbSucc2">0.5</parameter>',
-            '            </node>',
-            '            <node refinement="disjunctive">',
-            '                <label>find PIN</label>',
-            '                <parameter domainId="MinTimeSeq1">100.0</parameter>',
-            '                <parameter domainId="ProbSucc2">0.1</parameter>',
-            '            </node>',
-            '            <node refinement="conjunctive" switchRole="yes">',
-            '                <label>security training</label>',
-            '                <node refinement="disjunctive">',
-            '                    <label>learn rules</label>',
-            '                    <parameter domainId="ProbSucc2">0.7</parameter>',
-            '                </node>',
-            '                <node refinement="disjunctive">',
-            '                    <label>adhere to rules</label>',
-            '                    <parameter domainId="ProbSucc2">0.3</parameter>',
-            '                    <node refinement="disjunctive" switchRole="yes">',
-            '                        <label>threaten victim</label>',
-            '                        <parameter domainId="MinTimeSeq1">5.0</parameter>',
-            '                        <parameter domainId="ProbSucc2">0.9</parameter>',
-            '                    </node>',
-            '                </node>',
-            '            </node>',
-            '        </node>',
-            '        <node refinement="disjunctive">',
-            '            <label>get card</label>',
-            '            <parameter domainId="MinTimeSeq1">20.0</parameter>',
-            '            <parameter domainId="ProbSucc2">0.25</parameter>',
-            '        </node>',
-            '    </node>',
-            '    <domain id="MinTimeSeq1">',
-            '        <class>lu.uni.adtool.domains.predefined.MinTimeSeq</class>',
-            '        <tool>ADTool</tool>',
-            '    </domain>',
-            '    <domain id="ProbSucc2">',
-            '        <class>lu.uni.adtool.domains.predefined.ProbSucc</class>',
-            '        <tool>ADTool</tool>',
-            '    </domain>',
-            '</adtree>',
-        ]*/
         input_text = await getXML(); // Get XML string
         var json = JSON.parse(await build_json(input_text)); // Get JSON string and parse to JSON object
         return json;
