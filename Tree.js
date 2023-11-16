aa7332b24835a2ad090605c7cfe84fb4868298f8
Reshit Fazlija <reshitf@gmail.com>
2023-11-16 19:53:58 +0100

    Further improvements to dragging

--- Tree.js
+++ Tree.js
@@ -3,11 +3,9 @@ class ADTree{
     constructor(inputVal){
         this.name = "ROOT NODE!";
         this.root;
-        this.offsetX;
-        this.x = canvasElement.position().x;
-        this.offsetY;
-        this.y = canvasElement.position().y;
         this.isDragging = false;
+        this.oldX = canvasElement.position().x;
+        this.oldY = canvasElement.position().y;
         if (inputVal == null) { // if input NOT given, use nodeChildTextInput. Else, use input.
             let textVal = select("#nodeChildTextInput").elt.value;
             if (!textVal) // if nodeChildTextInput empty, use placeholder
@@ -41,6 +39,7 @@ class ADTree{
         console.log(this.name);
     }
     inputPressed(){
+        console.log(mouseX + canvasElement.position().x, mouseY + canvasElement.position().y, this.root.position().x, this.root.position().y)
         console.log("pressed");
         active = this;
         
@@ -49,9 +48,21 @@ class ADTree{
         active = null;
         console.log("unpressed");
         this.isDragging = false;
+        this.oldX = this.root.position().x;
+        this.oldY = this.root.position().y;
     }
     setPos(X,Y){
-        this.root.position(canvasElement.position().x+X-this.root.width/2,canvasElement.position().y+Y-this.root.height/2);
-        console.log(this.root.width/2);
+        if (
+            mouseX + canvasElement.position().x < this.oldX-10 ||
+            mouseX + canvasElement.position().x > this.oldX+this.root.width+10 ||
+            mouseY + canvasElement.position().y < this.oldY-10 ||
+            mouseY + canvasElement.position().y > this.oldY+this.root.height+10
+        ){
+            this.isDragging = true;
+            this.root.elt.blur();
+        }
+        if (this.isDragging == true)
+            this.root.position(canvasElement.position().x+X-this.root.width/2,canvasElement.position().y+Y-this.root.height/2);
+        
     }
 }
