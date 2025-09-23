eee25d0775599b63a4476cef011c08d9d19187da
Jayme Hebinck <jayme.hebinck@live.nl>
2025-09-23 09:24:22 +0200

    clicky clicky worky now clicky clicky worky worky

--- script.js
+++ script.js
@@ -935,7 +935,7 @@ function screenshotText(node) {
     }
 }
 
-// clicky clicky worky
+// clicky clicky worky worky
 function screenshotWalk(node) {
     if (node.root.position().x - (standardWidth / 2) < minX)
         minX = node.root.position().x - (standardWidth / 2);
